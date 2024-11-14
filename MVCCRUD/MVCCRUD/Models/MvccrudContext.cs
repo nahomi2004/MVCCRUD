b6c084f00283b08b0c179ff8c07869489b29814b
nahomi2004 <nahomicabrera2004@gmail.com>
2024-11-14 16:17:59 -0500

    creado y sirviendo

---
+++ MVCCRUD/MVCCRUD/Models/MvccrudContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD.Models;
+
+public partial class MvccrudContext : DbContext
+{
+    public MvccrudContext()
+    {
+    }
+
+    public MvccrudContext(DbContextOptions<MvccrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        // => optionsBuilder.UseSqlServer("server=Nahomi\\SQLEXPRESS; database=MVCCRUD; integrated security=true; TrustServerCertificate=Yes");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__usuarios__3213E83F6FF0F455");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Contrasenia)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("contrasenia");
+            entity.Property(e => e.Email)
+                .HasMaxLength(75)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Rol)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("rol");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVCCRUD/MVCCRUD/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCCRUD.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Contrasenia { get; set; }
+
+    public string? Rol { get; set; }
+}
